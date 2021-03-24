70f01a96a68b3ac3a2945aa172efd3d33a12b669
xiaoliu66 <lunhuilk@163.com>
2021-03-24 12:28:36 +0800

    #修复自定义时间bug

--- js/menu.js
+++ js/menu.js
@@ -1,39 +1,49 @@
 const { Menu, BrowserWindow } = require('electron')
-var {shell} = require('electron')
-var template = [{
-    label: '设置',
-    submenu: [{
-        label: '提示音',
-        accelerator: 'ctrl+n',
-        click: () => {
-            var win = new BrowserWindow({
-                width: 400,
-                height: 260,
-                webPreferences: { nodeIntegration: true,contextIsolation: false},
-                icon: './build/icon.ico'
-            })
-            // 取消菜单栏 不然会出现子窗口递归
-            win.setMenu(null)
-            win.loadFile("./html/sound.html")
-            win.on('closed', () => {
-                win = null
-            })
-        }
+var { shell } = require('electron')
+var template = [
+    {
+        label: '数据',
+        submenu: [
+            {
+                label: '数据导入',
+                
+            },
+            { label: '数据导出' }]
     },
-    { label: '' }]
-},
-
-{
-    label: '关于',
-    submenu: [
-        {
-            label: 'Github',
-            click: ()=>{
-                shell.openExternal("https://github.com/xiaoliu66")
+    {
+        label: '设置',
+        submenu: [
+            {
+                label: '提示音',
+                accelerator: 'ctrl+n',
+                click: () => {
+                    var win = new BrowserWindow({
+                        width: 400,
+                        height: 260,
+                        webPreferences: { nodeIntegration: true, contextIsolation: false },
+                        icon: './build/icon.ico'
+                    })
+                    // 取消菜单栏 不然会出现子窗口递归
+                    win.setMenu(null)
+                    win.loadFile("./html/sound.html")
+                    win.on('closed', () => {
+                        win = null
+                    })
+                }
+            },
+            { label: '夜间模式' }]
+    },
+    {
+        label: '关于',
+        submenu: [
+            {
+                label: 'Github',
+                click: () => {
+                    shell.openExternal("https://github.com/xiaoliu66")
+                }
             }
-        }
-    ]
-}
+        ]
+    }
 ]
 
 var m = Menu.buildFromTemplate(template)
